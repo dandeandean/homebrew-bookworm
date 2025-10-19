6c65a0a97a23e8109c9797a8bc9d5b8299f535c6
DanDeanDean <dpd5518@psu.edu>
2025-10-19 16:12:03 -0400

    add real binary to bookworm

--- Formula/bookworm.rb
+++ Formula/bookworm.rb
@@ -1,8 +1,9 @@
 class Cask < Formula
+  version "0.0.0"
   desc "Browser bookmarks, but better"
   homepage "https://github.com/dandeandean/bookworm"
-  url "https://github.com/dandeandean/bookworm"
-  sha256 ""
+  url "https://github.com/dandeandean/bookworm/releases/download/v#{version}/bookworm-macos-latest.zip"
+  sha256 "f329c83947f2c26a5664d47e7bcf81bd00e2efb7900e0d00917759aa9e34e1d3"
   license "MIT"
   head "https://github.com/dandeandean/bookworm", branch: "main"
 
